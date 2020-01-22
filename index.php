4dac4a9f34fde12c083eb080a60d43c30c54ed0f
Diego Caggiano <42618548+diegocaggiano@users.noreply.github.com>
2020-01-22 10:41:55 -0300

    Changing index.php to point to Logo file

--- index.php
+++ index.php
@@ -1,4 +1,9 @@
 <?php
+echo ' 
+<!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
+<html>
+<img src="logo.png" width="280" height="125" title="BASF Logo" alt="BASF Logo" />
 
-echo "Hello World!!!";
-echo "This is a web app made for BASF Services Americas SRL";
+</html>
+';
+?>
