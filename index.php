227df00c6fff0b77357d925fba8a2082ec9eb6bd
Diego Caggiano <42618548+diegocaggiano@users.noreply.github.com>
2020-01-22 12:20:06 -0300

    Small image

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 echo ' 
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 <html>
-<img src="logo.png" width="560" height="250" title="BASF Logo" alt="BASF Logo" />
+<img src="logo.png" width="280" height="125" title="BASF Logo" alt="BASF Logo" />
 
 </html>
 ';
